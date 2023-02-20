15ff93e264704cdb9d4c3526347bed5e8f0e703c
Daechler <50748803+Daechler@users.noreply.github.com>
2023-02-20 19:29:24 +0100

    Add files via upload

---
+++ src/main/java/net/daechler/lavavision/LavaVision.java
@@ -0,0 +1,51 @@
+package net.daechler.lavavision;
+
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LavaVision extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getLogger().info("LavaInvis plugin enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        getLogger().info("LavaInvis plugin disabled");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("lavavision")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                if (player.hasPermission("lavavision.use")) {
+                    player.sendMessage("Lava Vision enabled");
+                    for (int x = -30; x <= 30; x++) {
+                        for (int y = -30; y <= 30; y++) {
+                            for (int z = -30; z <= 30; z++) {
+                                if (player.getLocation().getBlock().getRelative(x, y, z).getType() == Material.LAVA) {
+                                    player.sendBlockChange(player.getLocation().getBlock().getRelative(x, y, z).getLocation(), Material.AIR.createBlockData());
+                                }
+                            }
+                        }
+                    }
+                    return true;
+                } else {
+                    player.sendMessage("You don't have permission to use this command.");
+                    return true;
+                }
+            } else {
+                sender.sendMessage("Only players can use this command.");
+                return true;
+            }
+        }
+        return false;
+    }
+}
